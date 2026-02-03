1e439d79ac2b6d42dff9775991a8656c3b4d7fe0
Sam Vente <savente93@proton.me>
2026-02-03 14:19:43 +0000

    remove --dry flag

--- ci/teamcity/Delft3D/dvc/GarbageCollection.kt
+++ ci/teamcity/Delft3D/dvc/GarbageCollection.kt
@@ -43,7 +43,7 @@ object DvcGarbageCollection : BuildType({
                 export AWS_ACCESS_KEY_ID='%s3_delft3d_testbench_accesskey%'
                 export AWS_SECRET_ACCESS_KEY='%s3_delft3d_testbench_secret%'
                 dvc config -l
-                dvc gc -aT --cloud --dry -v
+                dvc gc -aT --cloud -v -f
                 """
         }
     }
