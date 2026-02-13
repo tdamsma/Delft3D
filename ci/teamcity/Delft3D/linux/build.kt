e7f57e0d01baa1b7861098122786cb55356a8568
tersteegh <harm.tersteeg@deltares.nl>
2026-02-13 15:46:24 +0100

    set conan_home (keeps everything close)

--- ci/teamcity/Delft3D/linux/build.kt
+++ ci/teamcity/Delft3D/linux/build.kt
@@ -73,7 +73,7 @@ object LinuxBuild : BuildType({
             """.trimIndent()
         }
         script {
-          name = "Load, Build, Create, Push, Conan"
+          name = "Load, Build, Create, Push, Conan, Build"
           workingDir = "./"
           val recipedirectory = ""
           scriptContent = """
@@ -101,32 +101,41 @@ object LinuxBuild : BuildType({
               conan install --profile:build=tools/conan/default_linux --profile:host=tools/conan/default_linux --build=missing --output-folder=./build/ conanfile.py 
               ## pushback packages that we have created so that any update gets into nexus (We dont need the next person then to build it again :) )
               conan upload "*" -r deltaresconandev --confirm
+              set -eo pipefail
+              export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:${'$'}PKG_CONFIG_PATH
+              export LD_LIBRARY_PATH=/usr/local/lib:${'$'}LD_LIBRARY_PATH
+              export CMAKE_PREFIX_PATH=/usr/local:${'$'}CMAKE_PREFIX_PATH
+              export CMAKE_INCLUDE_PATH=/usr/local/include:${'$'}CMAKE_INCLUDE_PATH
+              export CMAKE_LIBRARY_PATH=/usr/local/lib:${'$'}CMAKE_LIBRARY_PATH
+              echo " cmake -S ./src/cmake -G %generator% -D CONFIGURATION_TYPE:STRING=%product% -D CMAKE_BUILD_TYPE=%build_type% -B build_%product% -D CMAKE_INSTALL_PREFIX=build_%product%/install  -D CMAKE_TOOLCHAIN_FILE=../build/build/%build_type%/generators/conan_toolchain.cmake"
+              cmake -S ./src/cmake -G %generator% -D CONFIGURATION_TYPE:STRING=%product% -D CMAKE_BUILD_TYPE=%build_type% -B build_%product% -D CMAKE_INSTALL_PREFIX=build_%product%/install  -D CMAKE_TOOLCHAIN_FILE=../build/build/%build_type%/generators/conan_toolchain.cmake
+              cmake --build build_%product% --parallel --config %build_type%
           """.trimIndent()
           dockerImage = "containers.deltares.nl/delft3d-dev/delft3d-third-party-libs:%dep.${LinuxThirdPartyLibs.id}.env.IMAGE_TAG%"
           dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
           dockerRunParameters = "--rm"
           dockerPull = true
         }
-        script {
-            name = "Build"
-            scriptContent = """
-                #!/usr/bin/env bash
-                source /etc/bashrc
-                set -eo pipefail
-                export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:${'$'}PKG_CONFIG_PATH
-                export LD_LIBRARY_PATH=/usr/local/lib:${'$'}LD_LIBRARY_PATH
-                export CMAKE_PREFIX_PATH=/usr/local:${'$'}CMAKE_PREFIX_PATH
-                export CMAKE_INCLUDE_PATH=/usr/local/include:${'$'}CMAKE_INCLUDE_PATH
-                export CMAKE_LIBRARY_PATH=/usr/local/lib:${'$'}CMAKE_LIBRARY_PATH
-                echo " cmake -S ./src/cmake -G %generator% -D CONFIGURATION_TYPE:STRING=%product% -D CMAKE_BUILD_TYPE=%build_type% -B build_%product% -D CMAKE_INSTALL_PREFIX=build_%product%/install  -D CMAKE_TOOLCHAIN_FILE=../build/build/%build_type%/generators/conan_toolchain.cmake"
-                cmake -S ./src/cmake -G %generator% -D CONFIGURATION_TYPE:STRING=%product% -D CMAKE_BUILD_TYPE=%build_type% -B build_%product% -D CMAKE_INSTALL_PREFIX=build_%product%/install  -D CMAKE_TOOLCHAIN_FILE=../build/build/%build_type%/generators/conan_toolchain.cmake
-                cmake --build build_%product% --parallel --config %build_type%
-            """.trimIndent()
-            dockerImage = "containers.deltares.nl/delft3d-dev/delft3d-third-party-libs:%dep.${LinuxThirdPartyLibs.id}.env.IMAGE_TAG%"
-            dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
-            dockerRunParameters = "--rm"
-            dockerPull = true
-        }
+//        script {
+//            name = "Build"
+//            scriptContent = """
+//                #!/usr/bin/env bash
+//                source /etc/bashrc
+//                set -eo pipefail
+//                export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:${'$'}PKG_CONFIG_PATH
+//                export LD_LIBRARY_PATH=/usr/local/lib:${'$'}LD_LIBRARY_PATH
+//                export CMAKE_PREFIX_PATH=/usr/local:${'$'}CMAKE_PREFIX_PATH
+//                export CMAKE_INCLUDE_PATH=/usr/local/include:${'$'}CMAKE_INCLUDE_PATH
+//                export CMAKE_LIBRARY_PATH=/usr/local/lib:${'$'}CMAKE_LIBRARY_PATH
+//                echo " cmake -S ./src/cmake -G %generator% -D CONFIGURATION_TYPE:STRING=%product% -D CMAKE_BUILD_TYPE=%build_type% -B build_%product% -D CMAKE_INSTALL_PREFIX=build_%product%/install  -D CMAKE_TOOLCHAIN_FILE=../build/build/%build_type%/generators/conan_toolchain.cmake"
+//                cmake -S ./src/cmake -G %generator% -D CONFIGURATION_TYPE:STRING=%product% -D CMAKE_BUILD_TYPE=%build_type% -B build_%product% -D CMAKE_INSTALL_PREFIX=build_%product%/install  -D CMAKE_TOOLCHAIN_FILE=../build/build/%build_type%/generators/conan_toolchain.cmake
+//                cmake --build build_%product% --parallel --config %build_type%
+//            """.trimIndent()
+//            dockerImage = "containers.deltares.nl/delft3d-dev/delft3d-third-party-libs:%dep.${LinuxThirdPartyLibs.id}.env.IMAGE_TAG%"
+//            dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
+//            dockerRunParameters = "--rm"
+//            dockerPull = true
+//        }
         script {
             name = "Run unit tests"
             scriptContent = """ 
