f2351e58c7b5d5c5f8b85bd2d8974fc11c961249
tersteegh <harm.tersteeg@deltares.nl>
2026-02-13 15:12:39 +0100

    update build and profile

--- ci/teamcity/Delft3D/linux/build.kt
+++ ci/teamcity/Delft3D/linux/build.kt
@@ -118,7 +118,8 @@ object LinuxBuild : BuildType({
                 export CMAKE_PREFIX_PATH=/usr/local:${'$'}CMAKE_PREFIX_PATH
                 export CMAKE_INCLUDE_PATH=/usr/local/include:${'$'}CMAKE_INCLUDE_PATH
                 export CMAKE_LIBRARY_PATH=/usr/local/lib:${'$'}CMAKE_LIBRARY_PATH
-                cmake -S ./src/cmake -G %generator% -D CONFIGURATION_TYPE:STRING=%product% -D CMAKE_BUILD_TYPE=%build_type% -B build_%product% -D CMAKE_INSTALL_PREFIX=build_%product%/install  -DCMAKE_TOOLCHAIN_FILE=../build/build/Release/generators/conan_toolchain.cmake
+                echo " cmake -S ./src/cmake -G %generator% -D CONFIGURATION_TYPE:STRING=%product% -D CMAKE_BUILD_TYPE=%build_type% -B build_%product% -D CMAKE_INSTALL_PREFIX=build_%product%/install  -D CMAKE_TOOLCHAIN_FILE=../build/build/%build_type%/generators/conan_toolchain.cmake"
+                cmake -S ./src/cmake -G %generator% -D CONFIGURATION_TYPE:STRING=%product% -D CMAKE_BUILD_TYPE=%build_type% -B build_%product% -D CMAKE_INSTALL_PREFIX=build_%product%/install  -D CMAKE_TOOLCHAIN_FILE=../build/build/%build_type%/generators/conan_toolchain.cmake
                 cmake --build build_%product% --parallel --config %build_type%
             """.trimIndent()
             dockerImage = "containers.deltares.nl/delft3d-dev/delft3d-third-party-libs:%dep.${LinuxThirdPartyLibs.id}.env.IMAGE_TAG%"
