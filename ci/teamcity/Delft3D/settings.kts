acb32436915452cc14506348f422edfa21625c51
huisman <matthijs.huisman@deltares.nl>
2026-04-03 12:10:44 +0000

    Fix TeamCity DVC testbench configuration
    Add DVC access key and secret parameters to TeamCity settings.
    Remove duplicate nearfield test case entry from testbench table.

--- ci/teamcity/Delft3D/settings.kts
+++ ci/teamcity/Delft3D/settings.kts
@@ -24,6 +24,9 @@ project {
         param("s3_dsctestbench_accesskey", DslContext.getParameter("s3_dsctestbench_accesskey"))
         password("s3_dsctestbench_secret", "credentialsJSON:7e8a3aa7-76e9-4211-a72e-a3825ad1a160")
 
+        param("dvc_testbench_accesskey", DslContext.getParameter("dvc_testbench_accesskey"))
+        password("dvc_testbench_secret", DslContext.getParameter("dvc_testbench_secret"))
+
         param("nexus_username", DslContext.getParameter("nexus_username"))
         password("nexus_password", DslContext.getParameter("nexus_password"))
         password("nexus_nuget_apikey", DslContext.getParameter("nexus_nuget_apikey"))
