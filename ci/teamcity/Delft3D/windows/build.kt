016e424f89ada0a2e57ee3fb6f55b8eb1ebf2bcb
Bart Steur <bart.steur@deltares.nl>
2026-04-28 14:38:14 +0200

    test build

--- ci/teamcity/Delft3D/windows/build.kt
+++ ci/teamcity/Delft3D/windows/build.kt
@@ -78,13 +78,7 @@ object WindowsBuild : BuildType({
                 call "C:\\Program Files (x86)\\Microsoft Visual Studio\\2022\\Community\\Common7\\Tools\\VsDevCmd.bat" -arch=x64 -host_arch=x64
                 call "C:\\Program Files (x86)\\Intel\\oneAPI\\setvars.bat"
 
-                where cl
-                where ifort
-                where ifx
-                where icx
-
-                cl
-                ifx --version
+                cmake -G "Visual Studio 17 2022" -A x64 -B test_build
 
                 cmake ./src/cmake -G %generator% -T fortran=%intel_fortran_compiler% -D CMAKE_BUILD_TYPE=%build_type% -D CONFIGURATION_TYPE:STRING=%product% -B build_%product% -D CMAKE_INSTALL_PREFIX=build_%product%/install -D ENABLE_CODE_COVERAGE=%enable_code_coverage_flag%
                 if %%errorlevel%% neq 0 exit /b %%errorlevel%%
