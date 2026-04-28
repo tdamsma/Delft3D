6f0ba1af9ee295e6313af083627d9efd25a2bc48
Bart Steur <bart.steur@deltares.nl>
2026-04-28 13:19:16 +0200

    test new setvars

--- ci/dockerfiles/windows/set-env-vs2022.cmd
+++ ci/dockerfiles/windows/set-env-vs2022.cmd
@@ -1,5 +1,6 @@
 @echo off
-call "C:\Program Files (x86)\Intel\oneAPI\setvars.bat"
-call "C:\Program Files (x86)\Intel\oneAPI\setvars-vcvarsall.bat"
-call "C:\Program Files (x86)\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
-cmd /K
+
+call "C:\Program Files (x86)\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" || exit /b %errorlevel%
+call "C:\Program Files (x86)\Intel\oneAPI\setvars.bat" intel64 || exit /b %errorlevel%
+
+where cl >nul || exit /b 1
