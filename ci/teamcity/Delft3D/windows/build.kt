6b2c53633a57b5bf724e70be27ab8d2ee31781eb
Bart Steur <bart.steur@deltares.nl>
2026-04-28 14:31:45 +0200

    test

--- ci/teamcity/Delft3D/windows/build.kt
+++ ci/teamcity/Delft3D/windows/build.kt
@@ -75,7 +75,8 @@ object WindowsBuild : BuildType({
         script {
             name = "Build"
             scriptContent = """
-                call C:/set-env-vs2022.cmd
+                call "C:\\Program Files (x86)\\Microsoft Visual Studio\\2022\\Community\\Common7\\Tools\\VsDevCmd.bat" -arch=x64 -host_arch=x64
+                call "C:\\Program Files (x86)\\Intel\\oneAPI\\setvars.bat"
 
                 where cl
                 where ifort
@@ -84,7 +85,7 @@ object WindowsBuild : BuildType({
 
                 cl
                 ifx --version
-                
+
                 cmake ./src/cmake -G %generator% -T fortran=%intel_fortran_compiler% -D CMAKE_BUILD_TYPE=%build_type% -D CONFIGURATION_TYPE:STRING=%product% -B build_%product% -D CMAKE_INSTALL_PREFIX=build_%product%/install -D ENABLE_CODE_COVERAGE=%enable_code_coverage_flag%
                 if %%errorlevel%% neq 0 exit /b %%errorlevel%%
 
