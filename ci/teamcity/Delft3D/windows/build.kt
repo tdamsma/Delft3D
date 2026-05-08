e9d7bb5583e0206a17d5026034b6bc2ce36a2901
Bart Steur <bart.steur@deltares.nl>
2026-05-08 09:40:13 +0200

    try new image

--- ci/teamcity/Delft3D/windows/build.kt
+++ ci/teamcity/Delft3D/windows/build.kt
@@ -47,7 +47,7 @@ object WindowsBuild : BuildType({
 
     steps {
         mergeTargetBranch {
-            dockerImage = "containers.deltares.nl/teamcity_agent/vs2022-intel2025-optimized-without-roslyn:latest"
+            dockerImage = "containers.deltares.nl/teamcity_agent/vs2022-intel2025-optimized-without-roslyn2:latest"
             dockerImagePlatform = ScriptBuildStep.ImagePlatform.Windows
             dockerPull = true
         }
@@ -57,7 +57,7 @@ object WindowsBuild : BuildType({
                 filename ="""ci\\teamcity\\Delft3D\\windows\\scripts\\determineProduct.py"""
                 scriptArguments = "%product% %teamcity.build.branch% %teamcity.build.branch.is_default% %build.vcs.number% %teamcity.pullRequest.source.branch%"
             }
-            dockerImage = "containers.deltares.nl/teamcity_agent/vs2022-intel2025-optimized-without-roslyn:latest"
+            dockerImage = "containers.deltares.nl/teamcity_agent/vs2022-intel2025-optimized-without-roslyn2:latest"
             dockerImagePlatform = PythonBuildStep.ImagePlatform.Windows
             dockerPull = true
         }
@@ -68,7 +68,7 @@ object WindowsBuild : BuildType({
                 echo #define BUILD_NR "%build.vcs.number%" > checkout_info.h
                 echo #define BRANCH "%teamcity.build.branch%" >> checkout_info.h
             """.trimIndent()
-            dockerImage = "containers.deltares.nl/teamcity_agent/vs2022-intel2025-optimized-without-roslyn:latest"
+            dockerImage = "containers.deltares.nl/teamcity_agent/vs2022-intel2025-optimized-without-roslyn2:latest"
             dockerImagePlatform = ScriptBuildStep.ImagePlatform.Windows
             dockerPull = true
         }
@@ -85,7 +85,7 @@ object WindowsBuild : BuildType({
                 ctest --test-dir ./build_%product% --build-config %build_type% --output-junit ../unit-test-report-windows.xml --output-on-failure
                 if %%errorlevel%% neq 0 exit /b %%errorlevel%%
             """.trimIndent()
-            dockerImage = "containers.deltares.nl/teamcity_agent/vs2022-intel2025-optimized-without-roslyn:latest"
+            dockerImage = "containers.deltares.nl/teamcity_agent/vs2022-intel2025-optimized-without-roslyn2:latest"
             dockerImagePlatform = ScriptBuildStep.ImagePlatform.Windows
             dockerPull = true
             dockerRunParameters = "--memory %teamcity.agent.hardware.memorySizeMb%m --cpus %teamcity.agent.hardware.cpuCount%"
